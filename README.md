e0d0a5f562879f9f8c4673c8b40d4ce60251b80f
Matt Sullivan <matt@rive.app>
2020-12-08 13:57:00 -0800

    Adds start/stop animation example

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ A collection of repositories showing how to use the [Rive](http://beta.rive.app)
 
 ## Flutter
 
-Run ```flutter create .``` in a Flutter project directry to generate the platform support directories.
+Run ```flutter create .``` in a Flutter project directry to generate platform support files.
+
+### Examples
+
+1. [*Simple looping Animation*](https://github.com/mjohnsullivan/rive-by-example/tree/main/flutter/01_looping_animation) - continuously play a looping animation.
+2. [*Start/Stop Animation*](https://github.com/mjohnsullivan/rive-by-example/tree/main/flutter/02_start_stop_animation) - control starting and stopping a looping animation.
--- flutter/02_start_stop_animation/lib/main.dart
+++ flutter/02_start_stop_animation/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Looping Animation',
+      title: 'Start and Stop a Looping Animation',
       home: Scaffold(
         body: Center(
           child: MyAnimation(),
@@ -24,8 +24,10 @@ class MyAnimation extends StatefulWidget {
 }
 
 class _MyAnimationState extends State<MyAnimation> {
-  /// The artboard we'll use to play one of its animations
+  /// The artboard we'll use to play an animation
   Artboard _artboard;
+  SimpleAnimation _animation;
+  var _playing = true;
 
   @override
   void initState() {
@@ -43,14 +45,37 @@ class _MyAnimationState extends State<MyAnimation> {
       // Get the artboard containing the animation you want to play
       final artboard = file.mainArtboard;
       // Create a SimpleAnimation controller for the animation you want to play
-      // and attach it to the artboard
-      artboard.addController(SimpleAnimation('idle'));
+      // and attach it to the artboard. Keep a reference to the controller to
+      // start and stop it.
+      artboard.addController(_animation = SimpleAnimation('idle'));
       // Wrapped in setState so the widget knows the animation is ready to play
       setState(() => _artboard = artboard);
     }
   }
 
+  /// Toggle animation playback and button states
+  void _togglePlayback() =>
+      // SimpleAnimation has an isActive flag that can be used to start and
+      // stop/pause the animation's playback
+      setState(() => _animation.isActive = _playing = !_playing);
+
   @override
-  Widget build(BuildContext context) =>
-      _artboard != null ? Rive(artboard: _artboard) : Container();
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Flexible(
+          flex: 1,
+          child: _artboard != null ? Rive(artboard: _artboard) : Container(),
+        ),
+        Flexible(
+          flex: 1,
+          child: RaisedButton(
+            child: Text(_playing ? 'Stop' : 'Start'),
+            onPressed: _togglePlayback,
+          ),
+        ),
+      ],
+    );
+  }
 }
